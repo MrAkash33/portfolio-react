c5464b119402f0367d0ceee498a5879a1c1f0a57
Mr Akash <79826594+MrAkash33@users.noreply.github.com>
2022-10-11 16:40:06 +0530

    adding api

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,33 +1,40 @@
 import React, { useContext, useRef, useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
 import "./Contact.css";
-import emailjs from "@emailjs/browser";
 import { themeContext } from "../../Context";
+import Validation from "../../validation";
+import { ApiUrl } from "../../config/config";
 const Contact = () => {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   const form = useRef();
-  const [done, setDone] = useState(false)
-  const sendEmail = (e) => {
-    e.preventDefault();
+  const [user, setUser] = useState('')
+  const [email, setEmail] = useState('')
+  const [message, setMessage] = useState('')
 
-    emailjs
-      .sendForm(
-        "service_2mu5xtl",
-        "template_m5udu2c",
-        form.current,
-        "VLwg1ltOWvnCYAiK_"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-          setDone(true);
-          form.reset();
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    const valid = Validation(user,email,message)
+    if(valid){
+        sendEmailApi()
+    }
   };
+
+  const sendEmailApi = ( async () =>{
+    const data = {
+      "username":user,
+      "email":email,
+      "message":message
+    }
+    const response = await fetch(ApiUrl,{
+      'method': 'POST',
+      'headers': {
+        'Content-Type': 'application/json'
+      },
+      body:JSON.stringify(data)
+    })
+  })
 
   return (
     <div className="contact-form" id="contact">
@@ -45,12 +52,12 @@ const Contact = () => {
       </div>
       {/* right side form */}
       <div className="c-right">
-        <form ref={form} onSubmit={sendEmail}>
-          <input type="text" name="user_name" className="user"  placeholder="Name"/>
-          <input type="email" name="user_email" className="user" placeholder="Email"/>
-          <textarea name="message" className="user" placeholder="Message"/>
+        <form onSubmit={sendEmail} >
+          <input type="text" name="user_name" className="user"  placeholder="Name" onChange={(e)=>{setUser(e.target.value)}}/>
+          <input type="text" name="user_email" className="user" placeholder="Email" onChange={(e)=>{setEmail(e.target.value)}}/>
+          <textarea name="message" className="user" placeholder="Message" onChange={(e)=>{setMessage(e.target.value)}}/>
           <input type="submit" value="Send" className="button"/>
-          <span>{done && "Thanks for Contacting me"}</span>
+          {/* <span>{done && "Thanks for Contacting me"}</span> */}
           <div
             className="blur c-blur1"
             style={{ background: "var(--purple)" }}
